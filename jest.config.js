ffd46ad6bc302b79f7c31cb01c53b2d6e33c2d78
Riceball LEE <snowyu.lee@gmail.com>
2018-10-10 00:07:49 +0800

    * [bug] can not test jest

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 const {defaults} = require('jest-config');
 
 module.exports = {
+  "collectCoverage": true,
   "transform": {
     "^.+\\.jsx?$": "babel-jest",
     "^.+\\.tsx?$": "ts-jest",
@@ -8,20 +9,28 @@ module.exports = {
     // "\\.(gql|graphql)$": "jest-transform-graphql",
   },
   "testRegex": "(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$",
+  // moduleFileExtensions: ['js', 'json', 'ts', 'tsx', 'vue'],
   moduleFileExtensions: [...defaults.moduleFileExtensions, 'ts', 'tsx', 'vue'],
   "moduleNameMapper": {
-    // "src/(.*)": "<rootDir>/src/$1",
-    // "@libs/(.*)": __dirname + "/src/libs/$1",
-    // "@plugins/(.*)": __dirname + "/src/plugins/$1"
+    // support the same @ -> src alias mapping in source code
+    "^@/(.*)$": "<rootDir>/src/$1",
     "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/__mocks__/fileMock.js",
     // "\\.(css|less|sass|stylus)$": "<rootDir>/__mocks__/styleMock.js",
     "\\.(css|less|sass|stylus)$": "identity-obj-proxy",
   },
+  // serializer for snapshots
+  "snapshotSerializers": [
+    "<rootDir>/node_modules/jest-serializer-vue"
+  ],
   // "moduleDirectories": [
   //   "node_modules",
   //   "src"
   // ],
   "globals": {
+    "vue-jest": {
+      // "babelConfig": ".babelrc",
+      "tsConfig": __dirname + "/tsconfig.jest.json"
+    },
     "ts-jest": {
       "diagnostics": true,
       // "skipBabel": true,
